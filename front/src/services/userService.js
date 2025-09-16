52274125752c883a709ed488ce59f5b6c799d786
Riantsoa04 <riantsoaramanakoarivo@gmail.com>
2025-09-16 11:55:13 +0300

    configuration url front

--- front/src/services/userService.js
+++ front/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "app-dc9c9368-12cb-4a91-b2af-34df8039b0b8.cleverapps.io/api/user";
+const API_URL = "https://app-dc9c9368-12cb-4a91-b2af-34df8039b0b8.cleverapps.io/api/user";
 
 export async function getAllUsers() {
   const response = await axios.get(`${API_URL}/`);
